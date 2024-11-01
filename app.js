8434b86ec8135be7f09eb918980d8f585bc51f81
DesireeCost4 <desireec.dev@gmail.com>
2024-11-01 16:45:56 -0300

    restaurando rota raiz

--- app.js
+++ app.js
@@ -80,11 +80,7 @@ app.get("/", (req, res) => {
 //rotas
 app.use("/admin", admin);
 
-// Ao final do seu app.js
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+
 
 
 
